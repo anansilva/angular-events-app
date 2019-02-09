6a995e3feff6b2024f13d0740469e4d7b2a0dce8
Ana Nunes da Silva <ananunesdasilva@gmail.com>
2019-02-09 18:51:43 +0000

    Add child component event-thumbnail

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html' 
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr/>
+  </div>
+  <event-thumbnail [event]="event1"></event-thumbnail>
+  `
 })
-
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Event',
     date: '27/09/2019',
